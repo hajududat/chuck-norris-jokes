d09e7011ee958cee5df9b07b7958c62e20d0f543
Justin Brown <hey@addsomebrown.com>
2020-05-20 21:29:47 -0400

    Added error handling

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ function App() {
   const [data, setData] = useState({ value: [] });
   const [url, setUrl] = useState("https://api.icndb.com/jokes/random");
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsError(false);
       setIsLoading(true);
 
-      const result = await axios(url);
+      try {
+        const result = await axios(url);
 
-      setData(result.data);
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+
       setIsLoading(false);
     };
 
@@ -29,6 +36,14 @@ function App() {
       <div className="row">
         <div className="column">
           <div className="card">
+            {isError && (
+              <div>
+                <span role="img" aria-label="emoji">
+                  😭
+                </span>
+                Something went wrong...
+              </div>
+            )}
             {isLoading ? <div>Loading ...</div> : <div>{data.value.joke}</div>}
           </div>
         </div>
