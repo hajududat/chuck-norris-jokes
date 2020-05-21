160fac4edcab343bbc5919787ad053d6daab5dbe
Justin Brown <hey@addsomebrown.com>
2020-05-20 21:24:56 -0400

    Added loading indicator

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import axios from "axios";
 function App() {
   const [data, setData] = useState({ value: [] });
   const [url, setUrl] = useState("https://api.icndb.com/jokes/random");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
+
       const result = await axios(url);
 
       setData(result.data);
+      setIsLoading(false);
     };
 
     fetchData();
@@ -24,7 +28,9 @@ function App() {
     <div className="container">
       <div className="row">
         <div className="column">
-          <div className="card">{data.value.joke}</div>
+          <div className="card">
+            {isLoading ? <div>Loading ...</div> : <div>{data.value.joke}</div>}
+          </div>
         </div>
       </div>
       <div className="row">
