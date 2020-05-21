bdcf4302eba0ba8bfd9d6d84f406a1930b93238e
Justin Brown <hey@addsomebrown.com>
2020-05-20 20:03:54 -0400

    Fetching data

--- src/App.js
+++ src/App.js
@@ -2,24 +2,47 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({ value: [] });
+  const [randomJoke] = useState("random");
+  const [joke, setJoke] = useState("https://api.icndb.com/jokes/random");
 
   useEffect(() => {
-    axios
-      .get("https://api.icndb.com/jokes/random")
-      .then((result) => setData(result.data));
-  }, []);
+    const fetchData = async () => {
+      const result = await axios(joke);
+
+      setData(result.data);
+    };
+
+    fetchData();
+  }, [joke]);
+
+  function handleClick(e) {
+    e.preventDefault();
+    console.log("The button was clicked.");
+  }
 
   return (
     <div className="container">
       <div className="row">
         <div className="column">
-          <h1>{data.value.joke}</h1>
+          <div className="card">{data.value.joke}</div>
         </div>
       </div>
       <div className="row">
         <div className="column">
-          <button className="btn btn__primary">Click</button>
+          <button
+            type="button"
+            className="btn btn__primary"
+            onClick={
+              (() => setJoke(`https://api.icndb.com/jokes/${randomJoke}`),
+              handleClick)
+            }
+          >
+            Make Onions Cry
+            <span role="img" aria-label="emoji">
+              👉
+            </span>
+          </button>
         </div>
       </div>
     </div>
